f509bbfd3856bb83b725e6b63996a142cbc26d85
Derric Williams <derricw@gmail.com>
2018-07-10 23:31:40 -0700

    added bars function for testing luminance

--- asciisciit/lut.py
+++ asciisciit/lut.py
@@ -4,6 +4,7 @@ lut.py
 Look up tables for asciisciit.
 
 """
+from bisect import bisect
 
 # LUMINANCE LUTS
 LUM = {
@@ -18,3 +19,31 @@ LUM = {
 }
 
 # COLOR
+
+def bars(lut):
+    """ Draws bars using the specified lut.
+    """
+    chars, lums = LUM[lut.upper()]
+    chars = list(chars)
+    arr = list(range(0, 255, 2))
+    line = ""
+    for v in arr:
+        row = bisect(lums, v)
+        character = chars[row]
+        line += character
+    for _ in range(5):
+        print(line)
+
+
+def main():
+    import sys
+    if len(sys.argv) > 1:
+        lut = sys.argv[1]
+    else:
+        lut = 'simple'
+    bars(lut)
+
+if __name__ == '__main__':
+    main()
+
+
